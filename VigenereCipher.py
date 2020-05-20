d6145e3cd52a811c2cf4829ecb2e48d71a8b7064
Luis Alcaraz <lalcarazgt@gmail.com>
2020-05-19 23:30:09 -0400

    Added dot strings

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -3,22 +3,22 @@ class VigenereCipher:
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     def __init__(self, text, key):
-        """"""
+
         self.text = text.upper()
         self.key = key
 
     def edKey(self, key):
-        """"""
+        """The edKey converts the key to upper case letters"""
         self.key = key.upper()
         return self.key
 
     def text_length(self):
-        """"""
+        """text_length calculates the length of the user's text"""
         self.t_length = len(self.text)
         return self.t_length
 
     def expand_key(self, key):
-        """"""
+        """expand_key compares the length of the key to the user's text and makes the key equal or longer than the text"""
         # This function cycles the key and appends itself until its length is equal or longer than the encrypted text
         self.expanded_key = self.key
         self.key_length = len(self.expanded_key)
@@ -31,7 +31,8 @@ class VigenereCipher:
             return (self.expanded_key)
 
     def encrypt(self):
-        """"""
+        """Encrypts the text using expanded key supplied by the user, starting with the key letter and using the alphabet
+        to determin the numerical position of the letter."""
         key_pos = 0
         enc_text = ""
         K = self.edKey(self.key)
@@ -56,7 +57,9 @@ class VigenereCipher:
         return enc_text
 
     def decrypt(self):
-        """"""
+        """Decryption is performed by going to the row in the table corresponding to the key,
+        finding the position of the ciphertext letter in that row and then using the column's
+        label as the plaintext."""
         key_pos = 0
         enc_text = ""
         K = self.edKey(self.key)
--- base64Cipher.py
+++ base64Cipher.py
@@ -3,18 +3,19 @@ from base64 import *
 
 class base64cipher:
     def __init__(self, text):
-        """"""
         self.text = text
 
     def encrypt(self):
-        """"""
+        """Encrypt first converts the text supplied by the user into bytes, then encodes into base64 code using the base64
+        python library, and finally decodes it from bytes into plaintext"""
         bytes_text = bytes(self.text, 'utf-8')
         self.encode = b64encode(bytes_text)
         coded_message = self.encode.decode('utf-8')
         return coded_message
 
     def decrypt(self):
-        """"""
+        """Decrypt converts the base64 encryption supplied by the user using the base64 python library,
+        and then decodes the byte data into plaintext"""
         self.decode = b64decode(self.text)
         message = self.decode.decode('utf-8')
         return message
